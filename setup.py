68188f3d7e75bf1f3af70207ac5bd7929a8b7872
Evan W <evwike@gmail.com>
2020-06-04 21:26:01 -0500

    _

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SI_Attendance',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Evan',
+    author_email='',
+    description=''
+)
