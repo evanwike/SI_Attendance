6b260877854e5c57c1dc3f715bb87c6be9ae9d3f
Evan W <evwike@gmail.com>
2020-06-04 21:14:38 -0500

    Refactor and updated gitignore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import datetime
+import logging
+import xlsxwriter
+
+logging.basicConfig(filename='errors.log', level=logging.DEBUG)
 
 # TODO: Allow for dynamic file names
 RESPONSE_FILE = 'Responses.xlsx'
@@ -21,58 +25,62 @@ def format_course(course: str) -> str:
     return course.upper()
 
 
-def find_student(last: str, ID: str, course: str):
-    print(f'Searching for: {last, ID, course}')
-
-    last = format_name(last)
-    course = format_course(course)
-
-    # Found course in attendance book
-    if course in ATTENDANCE_WBK:
-        print(f'\tFound course {course} in attendance book.')
-        df = ATTENDANCE_WBK[course]
-
-        # 1. Search by ID in current course sheet
-        row = df.loc[df['ID'] == ID]
-
-        if not row.empty:
-            print(f'SUCCESS: Student found by ID {ID}.\n')
-            return course, row.index[0]
-        print('\tUnable to find student by ID in course sheet.')
-
-        # 2. Search by last name in current course sheet
-        row = df.loc[df['Name'].str.upper().str.contains(last)]
-
-        if not row.empty:
-            print(f'SUCCESS: Student found by last name.\n')
-            return course, row.index[0]
-        print(f'\tUnable to find student by last name in course sheet: {last}')
-
-    # Unable to find course in attendance book
-    else:
-        print(f'\tUnable to find course "{course}" in attendance book.')
-        print(f'\tSearching for student "{last}" in other sheets...')
-
-        # 3. Search other course sheets
-    print()
-
-
 def get_week(date) -> int:
     start_week = datetime.date(2020, 6, 8).isocalendar()[1]
     return date.week - start_week
 
 
+def find_student_in_sheet(last: str, ID: str, course: str):
+    sheet = ATTENDANCE_WBK[course]
+    last = format_name(last)
+
+    # 1. Search by ID in current course sheet
+    row = sheet.loc[sheet['ID'] == ID]
+
+    if not row.empty:
+        print(f'SUCCESS: Student found by ID: {ID}\n')
+        return row.index[0]
+    print('\tUnable to find student by ID in course sheet.')
+
+    # 2. Search by last name in current course sheet
+    row = sheet.loc[sheet['Name'].str.upper().str.contains(last)]
+
+    if not row.empty:
+        print(f'SUCCESS: Student found by last name: {last}\n')
+        return row.index[0]
+    print(f'\tUnable to find student by last name in course sheet: {last}')
+    print('\n')
+
+
 def parse(response):
     date, last, ID, course = response
-    student = find_student(last, ID, course)
+    course = format_course(course)
 
-    if student is not None:
-        course, row_ind = student
-        # Ignore first 2 columns
-        week = 2 + get_week(date)
+    print(f'Searching for: {last, ID, course}')
 
-        # Increment week in student row in corresponding course sheet
-        ATTENDANCE_WBK[course].iat[row_ind, week] += 1
+    if course in ATTENDANCE_WBK:
+        print(f'\tCourse found in attendance workbook: {course}')
+
+        row_index = find_student_in_sheet(last, ID, course)
+
+        # Student found in current sheet
+        if row_index is not None:
+            week = 2 + get_week(date)
+
+            # Increment week in student row in corresponding course sheet
+            ATTENDANCE_WBK[course].iat[row_index, week] += 1
+
+        # Unable to find student in sheet, search other sheets
+        else:
+            print(f'\tSearching for student "{last}" in other sheets...')
+            # TODO: This would be easier if I had the times for each session - can't assume same student in different
+            #  course because they could be in multiple sessions
+
+    else:
+        print(f'\tUnable to find course "{course}" in attendance workbook.')
+        # TODO: Find most probably course student belongs to
+
+    print()
 
 
 def main():
